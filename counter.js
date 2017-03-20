de03bd7a68f8fcaa75ff22bf75893babee9ad5be
mollyli <mollyli@eztravel.com.tw>
2017-03-20 14:51:21 +0800

    add react

--- counter.js
+++ counter.js
@@ -1,3 +1,7 @@
+import { createStore } from 'redux';
+import React from 'react';
+import ReactDom from 'react-dom';
+
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -7,6 +11,40 @@ const counter = (state = 0, action) => {
     default:
       return state;
   }
+}
+
+// const createStore = (reducer) => {
+//   let state;
+//   let listeners = [];
+//   const getState = () => state;
+//   const dispatch = (action) => {
+//     state = reducer(state, action);
+//     listeners.forEach(listener => listener());
+//   }
+//   const subscribe = (listener) => {
+//     listeners.push(listener);
+//     return listeners.filter(l => l !== listener);
+//   };
+//   dispatch({}); //初始化
+//   return { getState, dispatch, subscribe };
+// }
+
+const store = createStore(counter);
+
+const Counter = ({ value }) => (<h1>{value}</h1>);
+const render = () => {
+  ReactDom.render(<Counter value={store.getState()}/>, document.getElementById('root'));
+}
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+})
+
+const cancel = () => {
+  setTimeout(() => store.subscribe())
 }
 
 export default counter;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    // 'webpack-dev-server/client?http://localhost:3000',
+    path.join(__dirname, 'counter.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+}
